33512ba35a39d0585388374a4d85352620d307b0
lilichuan <luffy74123@gmail.com>
2023-05-20 16:49:18 +0800

    add main list item view

--- app/src/main/java/com/example/travaltaipei/ui/fragments/MainListFragment.kt
+++ app/src/main/java/com/example/travaltaipei/ui/fragments/MainListFragment.kt
@@ -1,12 +1,15 @@
 package com.example.travaltaipei.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.travaltaipei.R
 import com.example.travaltaipei.databinding.FragmentMainListBinding
+import com.example.travaltaipei.viewmodel.MyListViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,7 @@ private const val ARG_PARAM2 = "param2"
 class MainListFragment : Fragment() {
 
     lateinit var binding: FragmentMainListBinding
+    lateinit var viewModel: MyListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +37,11 @@ class MainListFragment : Fragment() {
         binding = FragmentMainListBinding.inflate(inflater)
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        viewModel = ViewModelProvider(this).get(MyListViewModel::class.java)
     }
 
     companion object {
